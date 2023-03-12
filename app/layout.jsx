b91cc79c0ee770eb4526bf9598748a05215fcb3b
ell-ska <ella@skagerstrom.se>
2023-03-12 21:39:16 +0100

    test

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,26 @@
-import Header from '/Header'
-import Footer from '/Footer'
-import '/css/variables.css'
-import '/css/base.css'
+import Header from './Header'
+import Footer from './Footer'
+import './css/variables.css'
+import './css/base.css'
 
 export const metadata = {
-	title: 'HeardleDupe',
-	description: "A dupe of Spotify's music quiz Heardle",
-	icons: {
-		icon: '/favicon.svg',
-	},
+  title: 'HeardleDupe',
+  description: "A dupe of Spotify's music quiz Heardle",
+  icons: {
+    icon: '/favicon.svg'
+  }
 }
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body>
-				<div className="container">
-					<Header></Header>
-					{children}
-					<Footer></Footer>
-				</div>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <body>
+        <div className="container">
+          <Header></Header>
+          {children}
+          <Footer></Footer>
+        </div>
+      </body>
+    </html>
+  )
 }
