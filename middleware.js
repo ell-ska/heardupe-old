a1c93f1d3c35655928cb00b7fdd4bbcdc94b0927
ell-ska <ella@skagerstrom.se>
2023-05-03 15:09:09 +0200

    add middleware

--- middleware.js
+++ middleware.js
@@ -1,11 +1,13 @@
 import { withAuth } from 'next-auth/middleware'
 
-export default withAuth({
-  pages: {
-    signIn: '/login',
-  },
-})
+export default withAuth(
+  {
+    pages: {
+      signIn: '/login',
+    }
+  }
+)
 
 export const config = {
-    matcher: ['/statistics']
+  matcher: ['/profile']
 }
